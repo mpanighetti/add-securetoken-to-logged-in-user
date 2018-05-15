b0041a181a83dd995582dc0110babf2cb44426c9
Mario Panighetti <mpanighetti@mac.com>
2018-05-15 11:05:17 -0700

    updated for 10.13.4
    • added root_check (exits if script not run as root)
    • removed unnecessary sudo-ing (since script now has to run as root)
    • added exit when $loggedInUser creds are not provided
    • removed securetoken_double_check function, verify step now runs in securetoken_add function for simpler workflow
    • removed pass_clear function, unset commands now run right after granting SecureToken
    • updated macOS minimum requirements (10.13.4+)

--- add-securetoken-to-logged-in-user.sh
+++ add-securetoken-to-logged-in-user.sh
@@ -4,15 +4,15 @@
 #
 #            Name:  add-securetoken-to-logged-in-user.sh
 #     Description:  Adds SecureToken to currently logged-in user, allowing that
-#                   user to unlock FileVault in macOS High Sierra. Uses
+#                   user to enable or use FileVault in macOS High Sierra. Uses
 #                   credentials from a GUI-created admin account $guiAdmin
 #                   (retrieves from a manually-created System keychain entry),
 #                   and prompts for current user's password.
 #                   https://github.com/mpanighetti/add-securetoken-to-logged-in-user
 #          Author:  Mario Panighetti
 #         Created:  2017-10-04
-#   Last Modified:  2018-03-12
-#         Version:  2.1
+#   Last Modified:  2018-05-15
+#         Version:  2.2
 #
 ###
 
@@ -30,10 +30,8 @@ guiAdmin="guiadmin-username"
 # If you want to prompt for this information instead of pulling from the
 # keychain, you can copy the osascript from securetoken_add to generate a new
 # prompt, and pass the result to $guiAdminPass.
-guiAdminPass=$("/usr/bin/sudo" "/usr/bin/security" find-generic-password -wa "$guiAdmin")
 # leave these values as-is
 loggedInUser=$("/usr/bin/stat" -f%Su "/dev/console")
-secureTokenCheck=$("/usr/bin/sudo" "/usr/sbin/sysadminctl" -adminUser "$guiAdmin" -adminPassword "$guiAdminPass" -secureTokenStatus "$loggedInUser" 2>&1)
 macosMinor=$("/usr/bin/sw_vers" -productVersion | "/usr/bin/awk" -F . '{print $2}')
 macosBuild=$("/usr/bin/sw_vers" -productVersion | "/usr/bin/awk" -F . '{print $3}')
 
@@ -45,11 +43,18 @@ macosBuild=$("/usr/bin/sw_vers" -productVersion | "/usr/bin/awk" -F . '{print $3
 
 # exit script if macOS < 10.13.3
 macos_check () {
-  if [[ "$macosMinor" -ne 13 ]] || [[ "$macosBuild" -lt 3 ]]; then
-    "/bin/echo" "❌ ERROR: Mac does not meet script requirements (macOS 10.13.3 or later). Update to the latest compatible macOS build, then run script again."
+  if [[ "$macosMinor" -ne 13 ]] || [[ "$macosBuild" -lt 4 ]]; then
+    "/bin/echo" "❌ ERROR: Mac does not meet script requirements (macOS 10.13.4 or later). Update to the latest compatible macOS build, then run script again."
     exit 70
   fi
 }
+
+
+# exit script if not runnning as root
+if [[ $EUID -ne 0 ]]; then
+    "/bin/echo" "❌ ERROR: This script must run as root."
+    exit 1
+fi
 
 
 # add SecureToken to $loggedInUser account to allow FileVault access
@@ -77,45 +82,39 @@ set myReply to final_pass
 EOT
     )
   if [[ "$loggedInUserPass" = "" ]]; then
-     "/bin/echo" "A password was not entered for $loggedInUser, unable to proceed. Try running the script again; if issue persists, a manual SecureToken add will be required."
+     "/bin/echo" "❌ ERROR: A password was not entered for $loggedInUser, unable to proceed. Try running the script again; if issue persists, a manual SecureToken add will be required."
+     exit 1
   else
-    "/usr/bin/sudo" "/usr/sbin/sysadminctl" \
+    guiAdminPass=$("/usr/bin/security" find-generic-password -wa "$guiAdmin" "/Library/Keychains/System.keychain")
+    "/usr/sbin/sysadminctl" \
       -adminUser "$guiAdmin" \
       -adminPassword "$guiAdminPass" \
       -secureTokenOn "$loggedInUser" \
       -password "$loggedInUserPass"
+    unset guiAdminPass
+    unset loggedInUserPass
   fi
-}
 
 
-securetoken_double_check () {
-  secureTokenCheck=$("/usr/bin/sudo" "/usr/sbin/sysadminctl" -adminUser "$guiAdmin" -adminPassword "$guiAdminPass" -secureTokenStatus "$loggedInUser" 2>&1)
+  # verify successful SecureToken add
+  secureTokenCheck=$("/usr/sbin/sysadminctl" -secureTokenStatus "$loggedInUser" 2>&1)
   if [[ "$secureTokenCheck" =~ "DISABLED" ]]; then
-    "/bin/echo" "❌ ERROR: Failed to add SecureToken to $loggedInUser for FileVault access."
-    pass_clear
+    "/bin/echo" "❌ ERROR: Failed to add SecureToken to $loggedInUser for FileVault access. Try running the script again; if issue persists, a manual SecureToken add will be required."
     exit 126
   elif [[ "$secureTokenCheck" =~ "ENABLED" ]]; then
     "/bin/echo" "✅ Verified SecureToken is enabled for $loggedInUser."
     cred_clear
   else
     "/bin/echo" "❌ ERROR: Unexpected result, unable to proceed. Try running the script again; if issue persists, a manual SecureToken add will be required."
-    pass_clear
     exit 1
   fi
 }
 
 
+# delete $guiAdmin credentials from System keychain
 cred_clear () {
-  "/bin/echo" "Removing stored credentials..."
-  "/usr/bin/sudo" "/usr/bin/security" delete-generic-password -a "$guiAdmin"
-  pass_clear
-}
-
-
-pass_clear () {
-  "/bin/echo" "Clearing password variables..."
-  unset guiAdminPass
-  unset loggedInUserPass
+  "/bin/echo" "Removing stored credentials for $guiAdmin..."
+  "/usr/bin/security" delete-generic-password -a "$guiAdmin" "/Library/Keychains/System.keychain"
 }
 
 
@@ -126,11 +125,12 @@ pass_clear () {
 
 # verify Mac meets system requirements
 macos_check
+root_check
+
 
 # add SecureToken to $loggedInUser if missing
-if [[ "$secureTokenCheck" =~ "DISABLED" ]]; then
+if [[ $("/usr/sbin/sysadminctl" -secureTokenStatus "$loggedInUser" 2>&1) =~ "DISABLED" ]]; then
   securetoken_add
-  securetoken_double_check
 else
   "/bin/echo" "✅ Verified SecureToken is enabled for $loggedInUser."
   cred_clear
